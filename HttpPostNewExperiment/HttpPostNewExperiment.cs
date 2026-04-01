119a27487169777166d4214ea3a79061bfd9d24c
Marius Roosenschoon <marius.roosenschoon@dimensiondata.com>
2026-04-01 13:33:41 +0200

    Updated response

--- HttpPostNewExperiment/HttpPostNewExperiment.cs
+++ HttpPostNewExperiment/HttpPostNewExperiment.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using HttpPostNewExperiment.Models;
 
 namespace HttpPostNewExperiment
 {
@@ -17,19 +18,36 @@ namespace HttpPostNewExperiment
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("C# HTTP trigger function is processing a request.");
 
             string name = req.Query["name"];
+            string surname = req.Query["surname"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
+            surname = surname ?? data?.surname;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            //string responseMessage = string.IsNullOrEmpty(name)
+            //    ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+            //    : $"Hello, {name} {surname}. This HTTP triggered function executed successfully.";
 
-            return new OkObjectResult(responseMessage);
+            //return new OkObjectResult(responseMessage);
+
+            string fullname = name + " " + surname;
+
+            Response json = new Response();
+            json.message_success = "true";
+            json.message_fullname = fullname;
+            var jsonResponse = JsonConvert.SerializeObject(json);
+
+            var response = new OkObjectResult(jsonResponse);
+            response.ContentTypes.Add("application/json");
+
+            log.LogInformation("Responding with: " + jsonResponse);
+            log.LogInformation("C# HTTP trigger function is complete.");
+
+            return response;
         }
     }
 }
